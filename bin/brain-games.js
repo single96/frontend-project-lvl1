64a0b2c806d138566343b0fad2092928243b49b9
single96 <jenq9620@gmail.com>
2021-02-17 23:50:10 +0100

    added login prompt script

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -4,3 +4,24 @@ document.write(greetings);
 }
 
 displayGreetings();
+
+const loginInfo = () => {
+let askUserName = prompt('What is your name?');
+let askUserPassword = prompt('Enter your password');
+
+
+do {
+    if(askUserName == 'alex' || askUserPassword == '123') {
+        alert('Welcome Home' + ' ' + askUserName);
+    }
+
+    else {
+        alert('Something went wrong');
+    }
+    
+   }
+
+   while (askUserName == true || askUserPassword == true);
+} 
+
+loginInfo();
